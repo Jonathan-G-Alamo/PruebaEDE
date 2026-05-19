35b4da8d2b71ef2d58fdb06ceb867af714829dd9
alumno1 <alumno1@gmail.com>
2026-05-19 07:54:45 +0200

    notification day exam

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Jonathan!");
+        System.out.println("Hello, Jonathan! Today is the exam.");
     }
 }
